e7817d0af48d7653ac3b0795b87fbb982f453e30
Bowen Zhao <bowenzhao093@outlook.com>
2023-09-14 16:16:40 +1000

    Pin feature complete
    Able to pin selected coin overview at top of table

--- src/component/landing/CoinsTable.js
+++ src/component/landing/CoinsTable.js
@@ -5,7 +5,12 @@ import axios from 'axios';
 import { Container } from '@mui/system';
 import {
     Box,
+    Card,
+    CardContent,
+    CardHeader,
+    Checkbox,
     Divider,
+    IconButton,
     LinearProgress,
     Pagination,
     Stack,
@@ -18,9 +23,15 @@ import {
     Typography,
 } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { formatCurrency } from '../header/GlobalData';
+import { formatCurrency, formatNum } from '../header/GlobalData';
 import { SparkLineChart } from '@mui/x-charts';
 import styled from '@emotion/styled';
+import PushPinIcon from '@mui/icons-material/PushPin';
+import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
+import MockMarketData from '../../MockMarketData.json';
+import CloseIcon from '@mui/icons-material/Close';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import TrendingDownIcon from '@mui/icons-material/TrendingDown';
 
 // Display coins market data in table with pagination
 const CoinsTable = () => {
@@ -29,6 +40,7 @@ const CoinsTable = () => {
     const [coins, setCoins] = useState([]);
     const [loading, setLoading] = useState(false);
     const [page, setPage] = useState(1);
+    const [pinList, setPinList] = useState([]);
 
     useEffect(() => {
         fetchCoinsTable();
@@ -38,11 +50,11 @@ const CoinsTable = () => {
     const fetchCoinsTable = async () => {
         try {
             setLoading(true);
-            const res = await axios.get(CoinsMarket(100, page));
+            //const res = await axios.get(CoinsMarket(100, page));
             //const res = await axios.get();
-            const data = res.data;
-            console.log(data);
-            setCoins(data);
+            //const data = res.data;
+            console.log(MockMarketData);
+            setCoins(MockMarketData);
             console.log(coins);
             setLoading(false);
         } catch (err) {
@@ -54,6 +66,26 @@ const CoinsTable = () => {
         paddingTop: 0,
         paddingBottom: 0,
     });
+
+    const handlePinCheckedEvent = (e, coin) => {
+        e.stopPropagation();
+        if (!pinList.includes(coin)) {
+            setPinList((prev) => [...prev, coin]);
+            console.log('coin pushed');
+        } else {
+            const newList = pinList.filter((currCoin) => currCoin !== coin)
+            setPinList(newList);
+            console.log('coin removed');
+        }
+    }
+    console.log(pinList);
+
+    // const pinListEvent = (id) =>{
+    //     setPinList((pinList) => [...pinList, id])
+    //     return(
+    //         <PushPinIcon />
+    //     )
+    // }
 
     return (
         <Container style={{ textAlign: 'center' }} maxWidth='lg'>
@@ -85,8 +117,66 @@ const CoinsTable = () => {
                                 window.scroll(0, 450);
                                 console.log(value);
                             }}
-
                         />
+
+                        <Stack
+                            direction={{ xs: 'column', sm: 'row' }}
+                            spacing={{ xs: 1, sm: 1, md: 4 }}
+                        >
+                            {pinList.map((coin) => {
+                                return (
+
+                                    <Card sx={{ maxWidth: 200, maxHeight: 75 }} key={coin.name}>
+                                        <CardHeader
+                                            avatar={<img src={coin.image} alt={coin.name} height='20' />}
+                                            action={
+                                                <IconButton aria-label="settings" onClick={(e) => handlePinCheckedEvent(e, coin)}>
+                                                    <CloseIcon />
+                                                </IconButton>
+                                            }
+                                            title={`${coin.name} ${coin.symbol}`}
+                                            sx={{ padding: 1, paddingBottom: 0 }}
+                                        />
+                                        <CardContent sx={{ padding: 1, paddingTop: 0 }}>
+                                            <Box sx={{
+                                                flexDirection: 'row',
+                                                justifyContent: 'space-evenly',
+                                                minWidth: 'md',
+                                                display: 'flex',
+                                                mr: 1,
+                                            }}>
+                                                <Typography variant='body2'>
+                                                    24h Vol:
+                                                </Typography>
+
+                                                <Stack
+                                                    direction="row"
+                                                    justifyContent="center"
+                                                    alignItems="center"
+                                                    spacing={1}
+                                                    style={{
+                                                        color:
+                                                            coin.price_change_percentage_24h_in_currency > 0
+                                                                ? 'rgb(14, 203, 129)'
+                                                                : 'red',
+                                                    }}
+                                                >
+                                                    {coin.price_change_percentage_24h_in_currency >= 0
+                                                            ? <TrendingUpIcon />
+                                                            : <TrendingDownIcon />}
+                                                    <Typography >
+                                                    {coin.price_change_percentage_24h.toFixed(2)}%
+                                                    </Typography>
+
+                                                </Stack>
+                                            </Box>
+                                        </CardContent>
+                                    </Card>
+                                )
+                            })
+                            }
+
+                        </Stack>
 
                         {/* Table section */}
                         <Table>
@@ -95,6 +185,7 @@ const CoinsTable = () => {
                             <TableHead>
                                 <TableRow>
                                     {[
+                                        'Pin',
                                         '#',
                                         'Coin',
                                         'Price',
@@ -118,13 +209,23 @@ const CoinsTable = () => {
                             {/* Table body */}
                             <TableBody>
                                 {coins.map((coin) => {
+
+
                                     return (
                                         <TableRow
                                             onClick={() => navigate(`/coin/${coin.id}`)}
                                             className={coin.id}
                                             key={coin.name}
-
                                         >
+                                            {/* Pin coin for overview*/}
+                                            <StyledTableCell>
+                                                <IconButton onClick={(e) => handlePinCheckedEvent(e, coin)}>
+                                                    {pinList?.includes(coin) ? <PushPinIcon />
+                                                        : <PushPinOutlinedIcon />}
+                                                </IconButton>
+                                            </StyledTableCell>
+
+
                                             {/* Market cap rank */}
                                             <StyledTableCell align='right'>
                                                 <Typography>{coin.market_cap_rank}</Typography>
@@ -153,12 +254,12 @@ const CoinsTable = () => {
                                                     </Typography>
                                                 </Stack>
                                             </StyledTableCell>
-                                                
+
                                             {/* Coin's current price */}
                                             <StyledTableCell align='right'>
                                                 {formatCurrency(coin.current_price, 2, true)}
                                             </StyledTableCell>
-                                            
+
                                             {/* Coin's 1h price change percentage */}
                                             <StyledTableCell
                                                 align='right'
@@ -174,7 +275,7 @@ const CoinsTable = () => {
                                                     %
                                                 </span>
                                             </StyledTableCell>
-                                            
+
                                             {/* Coin's 24h price change percentage */}
                                             <StyledTableCell
                                                 align='right'
@@ -192,7 +293,7 @@ const CoinsTable = () => {
                                                     %
                                                 </span>
                                             </StyledTableCell>
-                                            
+
                                             {/* Coin's 7d price change percentage */}
                                             <StyledTableCell
                                                 align='right'
@@ -208,17 +309,17 @@ const CoinsTable = () => {
                                                     %
                                                 </span>
                                             </StyledTableCell>
-                                            
+
                                             {/* Coin's market cap */}
                                             <StyledTableCell align='right'>
                                                 {formatCurrency(coin.market_cap, 0, false)}
                                             </StyledTableCell>
-                                            
+
                                             {/* Coin's total volume */}
                                             <StyledTableCell align='right'>
                                                 {formatCurrency(coin.total_volume, 0, false)}
                                             </StyledTableCell>
-                                            
+
                                             {/* Coin's chart of price change in 7d */}
                                             <StyledTableCell align='right' width={'150'}>
                                                 <Box sx={{ flexGrow: 1 }}>
